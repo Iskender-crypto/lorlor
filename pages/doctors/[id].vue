bdb7baa8f255d35dac432b8bee3a004b32ada174
Iskender-crypto <iskenderazamov@gmail.com>
2024-01-11 13:13:16 +0600

    commit

---
+++ pages/doctors/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {useDataStore} from "~/stores/data";
+
+const route = useRoute()
+const dataStore = useDataStore();
+const user = dataStore.items[0]
+const router = useRouter()
+</script>
+
+<template>
+<h1>tem</h1>
+</template>
